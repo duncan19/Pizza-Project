19f5bbd1bac7cb697865aaa1cac62b0135a5a677
Duncan Robbins <duncanrobbins19@gmail.com>
2019-08-30 16:41:16 -0700

    add more javascript determining cost of pizza as well as cleaned up spacing inside the html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,41 @@
 var i === 0
-if (i === 10) {
-  alert("this pizza will cost you $12")
-}
-if else(i <= 9){
-  alert("this pizza will cost you $8")
-}
-if else(i >= 11)
-  alert("this pizza will cost you $16")
-if (('mushrooms').checked) {
-            i += 1;
-        } else {
-            i += 0;
-        }
-
 
 $(document).ready(function() {
-  $(".sizeform").submit(function(event)
+  $(".toppingsform").submit(function(event){
+    if (('pepperoni').checked) {
+                i += 1;
+            } else {
+                i += 0;
+            }
+    if (('olives').checked) {
+                i += 1;
+            } else {
+                i += 0;
+            }
+    if (('peppers').checked) {
+                i += 1;
+            } else {
+                i += 0;
+            }
+    if (('extraCheese').checked) {
+                i += 1;
+            } else {
+                i += 0;
+            }
+    if (('mushrooms').checked) {
+                i += 1;
+            } else {
+                i += 0;
+            }
+
+      if (i === 7) {
+        alert("this pizza will cost you $12")
+      }
+      if else(i <= 6){
+        alert("this pizza will cost you $8")
+      }
+      if else(i >= 8){
+        alert("this pizza will cost you $16")
+      }
+
+  }
