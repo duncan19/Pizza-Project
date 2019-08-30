3e06498d940e32b40dcaf2989441a82ddca2f464
Duncan Robbins <duncanrobbins19@gmail.com>
2019-08-30 16:03:17 -0700

    start using i to define the cost of the pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
+var i === 0
+if (i === 10) {
+  alert("this pizza will cost you $12")
+}
+if else(i <= 9){
+  alert("this pizza will cost you $8")
+}
+if else(i >= 11)
+  alert("this pizza will cost you $16")
+if (('mushrooms').checked) {
+            i += 1;
+        } else {
+            i += 0;
+        }
+
+
 $(document).ready(function() {
-  $(".sizeform").submit(function(event) 
+  $(".sizeform").submit(function(event)
